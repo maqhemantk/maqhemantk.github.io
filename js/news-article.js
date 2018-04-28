38fc397f32e9f6d04f1c87ca2976a2d052679b58
maqhemantk <38578891+maqhemantk@users.noreply.github.com>
2018-04-28 15:37:16 +0530

    Add files via upload

--- js/news-article.js
+++ js/news-article.js
@@ -1,7 +1,8 @@
 ﻿/*jslint plusplus: true */
 "use strict";
 var oNewsArticlePager = {
-    template: '<div><div class="post-meta"><span>@date</span></div><div class="post-header"><a href="/news-article.html?title=@title"><h2>@title</h2></a><div class="social-media-share"><div class="fb-share-button share-button" data-href="@newslink" data-summary="summary" data-description="description" data-desc="desc" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=@fbunewslink&amp;src=sdkpreparse&amp;t=thisisdynamictitle"></a></div><a href="https://twitter.com/share?ref_src=twsrc%5etfw" data-via="maqsoftware" data-url="@twitternewslink" class="twitter-share-button share-button" data-text="@linktitle" data-show-count="true"></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script><span class="linkedin-share-button share-button"><script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_us</script><script type="in/share" data-counter="right"></script></span><a href="http://www.linkedin.com/sharearticle?mini=true&summary=@lnsummary&url=@lnurl&submitted-image-url=@newsimagesrc&image-url=https://vijayagowrisankar.files.wordpress.com/2017/12/23.png&image=https://vijayagowrisankar.files.wordpress.com/2017/12/23.png?w=611&title=@lntitle&source=@lnsource" rel="nofollow"></a><span onclick=facebookshare("http://stravisocapture.000webhostapp.com/fbtest.html","thisismycustomtitle","thisiscustomdescription","https://vijayagowrisankar.files.wordpress.com/2017/12/23.png?w=611")></span></div></div><div class="post-media"><img alt="News" title="@tooltip" src="@newsimagesrc"></div><div class="post-entry">@content</div></div>',
+    template: '<div> <div class="post-meta"><span>@date</span></div><div class="post-header" id="post-share"></div><div class="post-media"><img alt="News" title="@tooltip" src="@newsimagesrc"></div><div class="post-entry">@content</div></div>',
+    socialMediatemplate: "<h2>@title</h2> <div class='social-media-share'> <div class='fb-share-button share-button' data-href='@newslink' data-summary='summary' data-description='description' data-desc='desc' data-layout='button_count' data-size='small' data-mobile-iframe='true'> <a class='fb-xfbml-parse-ignore' target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=@fbunewslink&amp;src=sdkpreparse&amp;t=thisisdynamictitle'></a> </div><a href='https://twitter.com/share?ref_src=twsrc%5etfw' data-via='maqsoftware' data-url='@twitternewslink' class='twitter-share-button share-button' data-text='@linktitle' data-show-count='true'></a> <script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script><span class='linkedin-share-button share-button'><script src='//platform.linkedin.com/in.js' type='text/javascript'> lang: en_us</script><script type='in/share' data-counter='right'></script></span> <a href='http://www.linkedin.com/sharearticle?mini=true&summary=@lnsummary&url=@lnurl&submitted-image-url=@newsimagesrc&image-url=https://vijayagowrisankar.files.wordpress.com/2017/12/23.png&image=https://vijayagowrisankar.files.wordpress.com/2017/12/23.png?w=611&title=@lntitle&source=@lnsource' rel='nofollow'></a><span onclick=facebookshare( 'http://stravisocapture.000webhostapp.com/fbtest.html', 'thisismycustomtitle', 'thisiscustomdescription', 'https://vijayagowrisankar.files.wordpress.com/2017/12/23.png?w=611')></span></div>",
     pageIndex: 0,
     pagesize: 6
 }, id, highlightid, sClickedHighlightTitle, iClickedHighlightID,
@@ -11,6 +12,7 @@ iMaxPageIndex,
 oNewsData = null,
 oEntryData = null,
 oNewsContainer = $("#LoadPageNews"),
+oShareContainer = $(".post-header"),
 sScrollElement = "body,html",
 sLoadingClass = "Loading",
     oItalicBookName = [
@@ -19,7 +21,7 @@ sLoadingClass = "Loading",
                     , "What I Did Not Learn at IIT - Transitioning from Campus to Workplace"
     ], iCount, iTotal = oItalicBookName.length, iTotalHighlight = 6, oNewsHighlightTitle = [iTotalHighlight], oHighlightNewsID = [iTotalHighlight];
 var newsTitle;
-function readQueryParams() {
+function readQueryParams() { 
     var params = (new URL(document.location)).searchParams;
     newsTitle = params.get("title");
     if (typeof newsTitle !== 'undefined' && newsTitle !== 'null' && newsTitle !== "" && newsTitle !== 'false') {
@@ -71,54 +73,56 @@ function loadNews(sNewsData) {
 function renderNews() {
     var iStart, iEnd, entry1, sDate, oDatePart, oDate, sTitle, sContent, sRawTitle;
     if (iTotalNews) {
+        
+            entry1 = oEntryData;
+            if (entry1) {
+                sDate = entry1.getElementsByTagName('published')[0].childNodes[0].nodeValue.toLowerCase().split("t");
+                oDatePart = [];
+                if (sDate[0]) {
+                    oDatePart = sDate[0].split("-");
+                }
+                oDate = null;
+                if (oDatePart.length === 3) {
+                    oDate = new Date(oDatePart[0], (oDatePart[1] - 1), oDatePart[2]);
+                } else {
+                    oDate = new Date(sDate[0]);
+                }
+                oDate = oDate.format();
+                sTitle = entry1.getElementsByTagName('title')[0].childNodes[0].nodeValue;
+                sRawTitle = sTitle;
+                sContent = entry1.getElementsByTagName('content')[0].childNodes[0].nodeValue;
+                for (iCount = 0; iCount < iTotal; iCount++) {
+                    sTitle = sTitle.replace(oItalicBookName[iCount], "<i class='SemiBold'>" + oItalicBookName[iCount] + "</i>");
+                }
+                $("#bloggerContent").html(sContent);
+                var bloggerContent = document.getElementById("bloggerContent");
+                var imgs = bloggerContent.getElementsByTagName('img');
+                var img, src = "";
 
-        entry1 = oEntryData;
-        if (entry1) {
-            sDate = entry1.getElementsByTagName('published')[0].childNodes[0].nodeValue.toLowerCase().split("t");
-            oDatePart = [];
-            if (sDate[0]) {
-                oDatePart = sDate[0].split("-");
+                if (typeof imgs !== "undefined" && imgs.length > 0) {
+                    img = imgs[0];
+                    src = img.src;
+                    img.parentNode.removeChild(img);
+                }
+                sContent = $("#bloggerContent").html();
+                oNewsContainer.addClass(sLoadingClass);
+                oNewsContainer.append(oNewsArticlePager.template.replace("@date", oDate).replace("@content", sContent).replace(/@newsimagesrc/g, src).replace("@tooltip", getFirstNWordsWithEllipses(sRawTitle, 4)));
+                document.getElementsByTagName('meta')['og:image'].setAttribute('content', src);
+                document.getElementsByTagName('meta')['twitter:image:src'].setAttribute('content', src);
+                $(".post-header").append(oNewsArticlePager.socialMediatemplate.replace(/@title/g, sTitle).replace(/@newsimagesrc/g, src).replace("@fbunewslink", "https://maqsoftware.com/news?article=" + sRawTitle).replace("@newslink", "https://maqsoftware.com/news?article=" + sRawTitle).replace("@twitternewslink", encodeURI("https://maqsoftware.com/news?article=" + sRawTitle)).replace("@linkedinnewslink", encodeURI("https://maqsoftware.com/news?article=" + sRawTitle))
+                                    .replace("@lnurl", encodeURI("https://maqsoftware.com/news?article=" + sRawTitle))
+                                    .replace("@linktitle", sRawTitle.substr(0, 200))
+                                    .replace("@lnsummary", sRawTitle.substr(0, 256))
+                                    .replace("@lntitle", sRawTitle)
+                                    .replace("@lnsource", "MAQ Software"));
             }
-            oDate = null;
-            if (oDatePart.length === 3) {
-                oDate = new Date(oDatePart[0], (oDatePart[1] - 1), oDatePart[2]);
-            } else {
-                oDate = new Date(sDate[0]);
-            }
-            oDate = oDate.format();
-            sTitle = entry1.getElementsByTagName('title')[0].childNodes[0].nodeValue;
-            sRawTitle = sTitle;
-            sContent = entry1.getElementsByTagName('content')[0].childNodes[0].nodeValue;
-            for (iCount = 0; iCount < iTotal; iCount++) {
-                sTitle = sTitle.replace(oItalicBookName[iCount], "<i class='SemiBold'>" + oItalicBookName[iCount] + "</i>");
-            }
-            $("#bloggerContent").html(sContent);
-            var bloggerContent = document.getElementById("bloggerContent");
-            var imgs = bloggerContent.getElementsByTagName('img');
-            var img, src = "";
-
-            if (typeof imgs !== "undefined" && imgs.length > 0) {
-                img = imgs[0];
-                src = img.src;
-                img.parentNode.removeChild(img);
-            }
-            sContent = $("#bloggerContent").html();
-            oNewsContainer.addClass(sLoadingClass);
-            oNewsContainer.append(oNewsArticlePager.template.replace(/@title/g, sTitle).replace("@date", oDate).replace("@content", sContent).replace(/@newsimagesrc/g, src).replace("@tooltip", getFirstNWordsWithEllipses(sRawTitle, 4)).replace("@fbunewslink", "https://maqsoftware.com/news-article?title=" + sRawTitle).replace("@newslink", "https://maqsoftware.com/news-article?title=" + sRawTitle).replace("@twitternewslink", encodeURI("https://maqsoftware.com/news-article?title=" + sRawTitle)).replace("@linkedinnewslink", encodeURI("https://maqsoftware.com/news-article?title=" + sRawTitle))
-                                .replace("@lnurl", encodeURI("https://maqsoftware.com/news-article?title=" + sRawTitle))
-                                .replace("@linktitle", sRawTitle.substr(0, 200))
-                                .replace("@lnsummary", sRawTitle.substr(0, 256))
-                                .replace("@lntitle", sRawTitle)
-                                .replace("@lnsource", "MAQ Software"));
-        }
-
+        
         oNewsContainer.find("a").attr("target", "_blank");
         oNewsContainer.find("img").addClass("post - media");
 
-        document.getElementsByTagName('meta')['og:image'].setAttribute('content', src);
-        document.getElementsByTagName('meta')['twitter:image:src'].setAttribute('content', src);
+       
         $('#LoadPageNewsArticle *').removeAttr('style');
-
-
+        
+       
     }
 }
